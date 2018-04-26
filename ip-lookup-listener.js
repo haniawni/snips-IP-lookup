55b2da23233db5cbf38896b64f8ffde277e2feb5
Hani Awni <hani.awni@gmail.com>
2018-04-25 21:24:31 -0400

    init

---
+++ ip-lookup-listener.js
@@ -0,0 +1,35 @@
+var ip = require("ip");
+var mqtt = require('mqtt');
+var mqtt_options = {
+	servers: [{
+		host: 'localhost',
+		port: 1883
+	}],
+	will: {
+		topic: "/snips/ip-lookup/RIP",
+		payload: "ip-lookup lost connection",
+		qos: 0,
+		retain: 0
+	}
+};
+var client = mqtt.connect(mqtt_options);
+
+function endDialogue(sessionId,text){
+	client.publish('hermes/dialogueManager/endSession',JSON.stringify(resp));
+}
+
+function intentCallback(topic, msg){
+	var body = JSON.parse(msg);
+	endDialogue(body.sessionId,"My I.P.: " + ip.address());
+}
+
+client.on('connect', function() {
+	console.log("IP-lookup: CONNECTED & SUBSCRIBED SUCCESSFULLY");
+	client.subscribe('hermes/intent/haniawni:IpLookup');
+});
+
+client.on('error',function(err){
+	console.log("ERROR: "+err);
+});
+
+client.on('message', intentCallback);
